9d083098d8fe82f894bd1ca2b4948994fb71ae32
AhmedAbozaid <ae826252@gmail.com>
2022-11-30 15:18:16 +0200

    database

---
+++ Database.py
@@ -0,0 +1,47 @@
+import json
+from pymongo import MongoClient
+import csv
+from collections import Counter 
+import requests
+
+
+class db_pipeline:
+    def __init__(self, link, from_cluster, collection_name):
+        self.db = self.connect_db(link, from_cluster, collection_name)
+## connection to MongoDB Cloud Atlas
+    def connect_db(self, link, from_cluster, collection_name):
+        cluster = MongoClient(link)
+        db = cluster[from_cluster]
+        return db[collection_name]
+
+## def to import data from csv file into mongodb 
+    def push_csv(self, csv_file_path):
+        with open(csv_file_path,encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.db.insert_one(row)
+                print(row)
+##-------------------------------------------------------------------------------------------
+    def remove_all(self):
+        self.db.delete_many({})
+##-------------------------------------------------------------------------------------------
+class plates_db_api:
+    def __init__(self):
+        self.url = "https://data.mongodb-api.com/app/data-eqwcz/endpoint/data/v1/action/findOne"
+    def query(self, plate_number):
+        payload = json.dumps({
+            "collection": "plates",
+            "database": "Test",
+            "dataSource": "Cluster0",
+            "filter": {"plate": {"$regex": f"{plate_number}", "$options": "$i"},
+            }})
+        headers = {
+             'Content-Type': 'application/json',
+             'Access-Control-Request-Headers': '*',
+             'api-key': "Kq9DlhcEHxGY5GoKLMAhAkotJOoSeHnbVIsBpEcbsnsepxG5qrPikWrerjRpDG4a"
+              }
+
+        response = requests.request("POST", self.url, headers=headers, data=payload)
+        return response.text
+
+
